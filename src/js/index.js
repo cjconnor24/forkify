a0a849e75675866a06009d14ba3c89d2201a7dbf
Chris Connor <chris@chrisconnor.co.uk>
2018-09-05 16:14:01 +0100

    Adding some functionality for getting down recipies for testing. Also started to use Axio

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,7 @@
-import axios from 'axios';
-// http://food2fork.com/api/search
-const key = 'ea502601cf97c38abd12273a56da68ed';
-const proxy = 'https://crossorigin.me/';
+import Search from './models/Search';
 
-async function getResults(query){
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
 
- const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=ea502601cf97c38abd12273a56da68ed&q=pizza`);
- console.log(res);
 
- return res;
-
-}
-getResults('pizza').then(data => {
-    console.log(data);
-});
-console.log('hello?');
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,34 @@
-export default 'I am an exported string';
+import axios from 'axios';
+
+export default class Search {
+
+    
+    constructor(query){
+        this.query = query;
+    }
+
+// http://food2fork.com/api/search
+
+// const proxy = 'https://crossorigin.me/';
+
+async getResults(){
+
+    const key = 'ea502601cf97c38abd12273a56da68ed';
+
+    try {
+
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+        this.result = res.data.recipes;
+        
+        // console.log(this.result);
+        
+    } catch (error) {
+        
+        console.log('There was an error');
+
+    }
+
+}
+
+
+}
