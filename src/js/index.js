7a373cfa73da6073c71be9a97b837ecc300c9d87
Chris Connor <chris@chrisconnor.co.uk>
2018-09-05 16:29:04 +0100

    Started some of the controller functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,44 @@
 import Search from './models/Search';
 
+// GLOBAL APPLICATION STATE
+
+/**
+ * Search Object
+ * Current Recipe Object
+ * Shopping List Object
+ * Liked Recipes
+ */
+const state = {
+
+};
+
+/**
+ * Search functional
+ */
+const controlSearch = async () => {
+    // GET QUERY FROM VIEW
+    const query = 'pizza';
+
+    //CREATE NEW SEARCH OBJECT
+    if(query){
+        // NEW SEARCH OBJECT AND ADD IT TO STATE
+        state.search = new Search(query);
+
+        // PREPARE THE INTERFACE - LOADING SPINNER ETC.
+
+        // SEARCH FOR RECIPES
+        await state.search.getResults();
+        
+        // RENDER RESULTS ON UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
 const search = new Search('pizza');
 console.log(search);
 search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -17,7 +17,7 @@ async getResults(){
 
     try {
 
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+        const res = await axios(`Xhttps://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
         this.result = res.data.recipes;
         
         // console.log(this.result);
